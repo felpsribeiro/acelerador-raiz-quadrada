0638c0e4126958c5867cfe0b10589759bf683397
Felipe Nascimento Ribeiro <felipemch14@gmail.com>
2022-06-02 23:17:45 -0300

    refactor names

--- part2/part2.sv
+++ part2/part2.sv
@@ -1,40 +1,45 @@
 module part2(
-    // -------------------- Entradas -------------------------------------
+    // -------------------- Inputs -------------------------------------
     input   KEY[0],                         // RESET
     input   KEY[1],                         // CLOCK     
     input   KEY[3],                         // DATA_READY       
     input   [7:0] SW,                       // DATA_IN
-    // -------------------- Saidas ---------------------------------------
-    output  LEDG[0],                        // saida DONE
-    output  [6:0] HEX0, HEX1, HEX2, HEX3,   // valor de entrada da raiz
-    output  [6:0] HEX4, HEX5,               // valor do resultado da raiz
-    output  [6:0] HEX6, HEX7                // quantidades de ciclos
+    // -------------------- Outputs ---------------------------------------
+    output  LEDG[0],                        // DONE
+    output  [6:0] HEX0, HEX1, HEX2, HEX3,   // DATA_IN
+    output  [6:0] HEX4, HEX5,               // DATA_OUT
+    output  [6:0] HEX6, HEX7                // CYCLES
 );
 
-    // -------------------- Registradores --------------------------------
-    logic [7:0] entrada = SW;
-    logic [3:0] resultado = 0;
-    logic [3:0] ciclos = 0;
+    // -------------------- Registers ------------------------------------
+    logic [7:0] data_in = SW;
+    logic [3:0] data_out = 0;
+    logic [3:0] cycles = 0;
     logic       done;
-    logic [8:0] comando;
+    logic [8:0] states_controler;
 
-    // -------------------- Calcular Raiz --------------------------------
-    controler c (.clock(KEY[1]), .reset(KEY[0]), .flag(done), .saida(comando));
-    raiz #(.n(8)) r (.data_in(entrada), .controle(comando), .clock(KEY[1]), .reset(KEY[0]), .raiz(resultado), .ciclos(ciclos), .done(done));
 
-    // -------------------- Setar Saidas ---------------------------------
-    // sai­da DONE
+    // -------------------- Calculate Root --------------------------------
+    controler c (.clock(KEY[1]), .reset(KEY[0]), .done(done), .out(states_controler));
+    raiz #(.n(8)) r (.data_in(data_in), .controler(states_controler), .clock(KEY[1]), .reset(KEY[0]), .data_out(data_out), .cycles(cycles), .done(done));
+
+
+    // -------------------- Set Outputs -----------------------------------
+    // done
     assign LEDG = done;
-    // exibir valor da entrada
-    dec ent0(.v(entrada % 10),          .d(HEX0));  // digito 0
-    dec ent1(.v((entrada % 100) / 10),  .d(HEX1));  // digito 1
-    dec ent3(.v(entrada / 100),         .d(HEX2));  // digito 2
-    dec ent3(.v(15),                    .d(HEX3));  // apagar display
-    // exibir valor do resultado
-    dec res0(.v(resultado % 10), .d(HEX4));         // digito 0
-    dec res1(.v(resultado / 10), .d(HEX5));         // digito 1
-    // exibir quantidade de ciclos
-    dec cic0(.v(ciclos % 10), .d(HEX4));            // digito 0
-    dec cic1(.v(ciclos / 10), .d(HEX5));            // digito 1
+
+    // data_in
+    dec ent0(.v(data_in % 10),          .d(HEX0));
+    dec ent1(.v((data_in % 100) / 10),  .d(HEX1));
+    dec ent3(.v(data_in / 100),         .d(HEX2));
+    dec ent3(.v(15),                    .d(HEX3));
+
+    // data_out
+    dec res0(.v(data_out % 10), .d(HEX4));
+    dec res1(.v(data_out / 10), .d(HEX5));
+
+    // number of cycles
+    dec cic0(.v(cycles % 10), .d(HEX4));
+    dec cic1(.v(cycles / 10), .d(HEX5));
 
 endmodule
